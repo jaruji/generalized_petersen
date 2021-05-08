d306bdfe735df5f109a6b5f3af4f499f411ce395
marekkocurik <marek.kocurik02@gmail.com>
2021-05-08 16:55:10 +0200

    updated function to find hamiltonian cycles

--- main.cpp
+++ main.cpp
@@ -10,69 +10,54 @@ Zadanie cislo 2 z GRA_I:
 Vyhladavanie hamiltonovskych cyklov v generalizovanych Petersonovych grafoch
 */
 
+bool edgeIsValid(int v, bool **matrix, int path[], int pos)
+{
+    if (matrix[path[pos-1]][v] == 0)
+        return 0;
+    for (int i = 0; i < pos; i++)
+        if (path[i] == v)
+            return 0;
+    return 1;
+}
 
-//=================================================================
+/* solve hamiltonian cycle problem */
+int findHamCycle(bool **matrix, int path[], int pos, int n)
+{
+    int sum = 0;
+    if (pos == n)
+    {
+        if (matrix[ path[pos-1] ][ path[0] ] == 1) {
+//            printf("Nasla sa cesta:\n");
+//            for(int k = 0; k < n; k++)
+//                printf("%d ", path[k]);
+//            printf("\n\n");
+            return 1;
+        }
+        else
+            return 0;
+    }
 
-/* algo from https://www.sanfoundry.com/cpp-program-find-hamiltonian-cycle/ */
-//void printSolution(int path[], int n)
-//{
-//    std::cout<<"Solution Exists: ";
-//    for (int i = 0; i < n; i++)
-//        std::cout<<path[i]<<"  ";
-//    std::cout<< path[0]<<std::endl;
-//}
-//
-//bool isSafe(int v, bool **graph, int path[], int pos)
-//{
-//    if (graph [path[pos-1]][v] == 0)
-//        return false;
-//    for (int i = 0; i < pos; i++)
-//        if (path[i] == v)
-//            return false;
-//    return true;
-//}
-//
-///* solve hamiltonian cycle problem */
-//bool hamCycleUtil(bool **graph, int path[], int pos, int n)
-//{
-//    if (pos == n)
-//    {
-//        if (graph[ path[pos-1] ][ path[0] ] == 1)
-//            return true;
-//        else
-//            return false;
-//    }
-//
-//    for (int v = 1; v < n; v++)
-//    {
-//        if (isSafe(v, graph, path, pos))
-//        {
-//            path[pos] = v;
-//            if (hamCycleUtil (graph, path, pos+1, n) == true)
-//                return true;
-//            path[pos] = -1;
-//        }
-//    }
-//    return false;
-//}
-//
-///* solves the Hamiltonian Cycle problem using Backtracking.*/
-//bool hamCycle(bool **graph, int n)
-//{
-//    int *path = new int[n];
-//    for (int i = 0; i < n; i++)
-//        path[i] = -1;
-//    path[0] = 0;
-//    if (hamCycleUtil(graph, path, 1, n) == false)
-//    {
-//        std::cout<<"\nSolution does not exist"<<std::endl;
-//        return false;
-//    }
-//    printSolution(path, n);
-//    return true;
-//}
+    for (int i = 1; i < n; i++)
+    {
+        if (edgeIsValid(i, matrix, path, pos))
+        {
+            path[pos] = i;
+            sum += findHamCycle(matrix, path, pos+1, n);
+            path[pos] = -1;
+        }
+    }
+    return sum;
+}
 
-//=================================================================
+/* solves the Hamiltonian Cycle problem using Backtracking.*/
+int numOfHamCycles(bool **matrix, int n)
+{
+    int *path = new int[n];
+    for (int i = 0; i < n; i++)
+        path[i] = -1;
+    path[0] = 0;
+    return findHamCycle(matrix, path, 1, n);
+}
 
 bool **setFixedMatrixPart(bool **matrix, int n){
     for(int i = 0; i < n; i++){
@@ -113,11 +98,6 @@ bool **generateMatrix(int n, int r){
     return matrix;
 }
 
-int findHamiltonianCycles(bool **matrix, int n){
-    //hamCycle(matrix, 2*n);
-    return 0;
-}
-
 void printMatrix(bool **matrix, int n){
     for(int i = 0; i < 2 * n; i++){
         for(int j = 0; j < 2 * n; j++){
@@ -128,21 +108,25 @@ void printMatrix(bool **matrix, int n){
 }
 
 int main(){
-    int n = 3;
-    while(n <= 12) {
+    int n = 3, cycles = 0;
+    while(n <= 40) {
         int r = 1;
         while(r < (int)ceil((double)n / 2.0)) {
             std::cout << "n: " << n << ", r: " << r << '\n';
             auto matrix = generateMatrix(n, r);
-            printMatrix(matrix, n);
-            findHamiltonianCycles(matrix, n);
+//            printMatrix(matrix, n);
+            cycles = numOfHamCycles(matrix, 2*n);
+            if(cycles == 0)
+                printf("Nema ziadne kruznice\n");
+            else
+                printf("Naslo sa %d kruznic\n", cycles/2);
             for (int i = 0; i < 2 * n; ++i) {
                 delete[] matrix[i];
             }
             delete[] matrix;
             r++;
         }
-        std::cout << "\n################################################\n";
+        std::cout << "################################################\n";
         n++;
     }
 }
